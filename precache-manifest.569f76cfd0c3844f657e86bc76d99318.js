bc063a810f12bc69396d63487fbf81fd566287aa
Negi <HNegi@spherasolutions.com>
2020-01-02 14:58:27 +0530

    Updates

---
+++ precache-manifest.569f76cfd0c3844f657e86bc76d99318.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57bc644775fe0344e7086eb84e9d9973",
+    "url": "/graphsapp/index.html"
+  },
+  {
+    "revision": "ebdbb8250b591d4a1283",
+    "url": "/graphsapp/static/css/main.092ce850.chunk.css"
+  },
+  {
+    "revision": "ebc45bc15474f8e3c9c1",
+    "url": "/graphsapp/static/js/2.0956b5e0.chunk.js"
+  },
+  {
+    "revision": "44bb13c376a360a26522c8368cf779a1",
+    "url": "/graphsapp/static/js/2.0956b5e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ebdbb8250b591d4a1283",
+    "url": "/graphsapp/static/js/main.4699f8ff.chunk.js"
+  },
+  {
+    "revision": "cc5c703c31fc96333a48",
+    "url": "/graphsapp/static/js/runtime-main.541ebafa.js"
+  }
+]);
